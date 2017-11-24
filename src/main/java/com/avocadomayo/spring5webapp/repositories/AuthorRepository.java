999e057f3e19e06e58e852e40a3930d123dcf840
Wendy Liu <jingyliu@paypal.com>
2017-11-23 22:26:21 -0800

    Added documentation

--- src/main/java/com/avocadomayo/spring5webapp/repositories/AuthorRepository.java
+++ src/main/java/com/avocadomayo/spring5webapp/repositories/AuthorRepository.java
@@ -3,7 +3,7 @@ package com.avocadomayo.spring5webapp.repositories;
 import com.avocadomayo.spring5webapp.model.Author;
 import org.springframework.data.repository.CrudRepository;
 
-/** Spring data will see this and wire this into Spring context for use.
+/** Spring data will see this and wire this into Spring context for use during runtime.
  * CRUD stands for create, read, update, delete - we can do all these without writing SQL simply by extending the
  * CrudRepository<T, ID> interface. A Spring Data Repository like CRUD delegates to a specialized Repository object
  * such that alternative storage implementations may be interchanged. */
