0c52ca4e1de26df3a07ded9fe5be08f6ec151c9f
Wendy Liu <jingyliu@paypal.com>
2017-11-23 22:53:50 -0800

    Modified DevBootStrap to initiate Publisher data entries at startup

--- src/main/java/com/avocadomayo/spring5webapp/bootstrap/DevBootStrap.java
+++ src/main/java/com/avocadomayo/spring5webapp/bootstrap/DevBootStrap.java
@@ -2,11 +2,16 @@ package com.avocadomayo.spring5webapp.bootstrap;
 
 import com.avocadomayo.spring5webapp.model.Author;
 import com.avocadomayo.spring5webapp.model.Book;
+import com.avocadomayo.spring5webapp.model.Publisher;
 import com.avocadomayo.spring5webapp.repositories.AuthorRepository;
 import com.avocadomayo.spring5webapp.repositories.BookRepository;
+import com.avocadomayo.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /** An initialization script to populate DB with some data. Adding @Component makes this a Spring Bean, so it will
  * get wired up by the Spring Context. */
@@ -15,15 +20,18 @@ public class DevBootStrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
     /** During runtime this will get created by the Spring framework. We will get Author and Book Repository
      * implementations from the Spring Data JPA.
      * @param authorRepository
      * @param bookRepository
      */
-    public DevBootStrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootStrap(AuthorRepository authorRepository, BookRepository bookRepository,
+                        PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -33,9 +41,16 @@ public class DevBootStrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private void initData() {
 
+        // Publishers
+        Publisher harperCollins = new Publisher("Harper Collins", "123 Main St.");
+        Publisher worx = new Publisher("Worx", "567 Richmond Ave.");
+
+        publisherRepository.save(harperCollins);
+        publisherRepository.save(worx);
+
         // Eric
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        Book ddd = new Book("Domain Driven Design", "1234", harperCollins);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
@@ -44,7 +59,7 @@ public class DevBootStrap implements ApplicationListener<ContextRefreshedEvent> 
 
         // Rod
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "23444", "Worx");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", worx);
         rod.getBooks().add(noEJB);
 
         authorRepository.save(rod);
