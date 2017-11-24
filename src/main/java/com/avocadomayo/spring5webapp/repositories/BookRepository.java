0a3089a4bb6eb851df335961792517f3ec95161a
Wendy Liu <jingyliu@paypal.com>
2017-11-23 22:14:35 -0800

    Extended CrudRepository interface with these two interfaces to allow CRUD ops to be performed on Author and Book objects

---
+++ src/main/java/com/avocadomayo/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,11 @@
+package com.avocadomayo.spring5webapp.repositories;
+
+import com.avocadomayo.spring5webapp.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+/** Spring data will see this and wire this into Spring context for use.
+ * CRUD stands for create, read, update, delete - we can do all these without writing SQL simply by extending the
+ * CrudRepository<T, ID> interface. A Spring Data Repository like CRUD delegates to a specialized Repository object
+ * such that alternative storage implementations may be interchanged. */
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
